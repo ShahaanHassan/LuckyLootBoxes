fe5eb2a4e1ad2f925f9c48b54f08721b49ff6fe4
Shahaan Hassan <shahaan.hassan@gmail.com>
2022-04-11 23:02:46 +0100

    Initial commit

---
+++ src/main/java/com/shmc/luckylootboxes/LuckyLootBoxes.java
@@ -0,0 +1,10 @@
+package com.shmc.luckylootboxes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LuckyLootBoxes implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/shmc/luckylootboxes/client/LuckyLootBoxesClient.java
@@ -0,0 +1,13 @@
+package com.shmc.luckylootboxes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class LuckyLootBoxesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
