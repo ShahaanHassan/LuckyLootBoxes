1de2bca9539977334f19b45c66225eb3c2e0244b
Shahaan Hassan <shahaan.hassan@gmail.com>
2022-04-16 16:24:00 +0100

    basic logic to dispense items from loot table

--- src/main/java/com/shmc/luckylootboxes/block/AbstractLootBoxBlock.java
+++ src/main/java/com/shmc/luckylootboxes/block/AbstractLootBoxBlock.java
@@ -1,10 +1,78 @@
 package com.shmc.luckylootboxes.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.context.LootContext;
+import net.minecraft.loot.context.LootContextTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public class AbstractLootBoxBlock extends Block {
+import java.util.Objects;
+import java.util.Random;
 
-  public AbstractLootBoxBlock(Settings settings) {
+import static com.shmc.luckylootboxes.LuckyLootBoxes.MOD_ID;
+
+public abstract class AbstractLootBoxBlock extends Block implements BlockEntityProvider {
+
+  private final Random seed;
+
+  protected AbstractLootBoxBlock(Settings settings) {
     super(settings);
+    this.seed = new Random();
+  }
+
+  @Override
+  public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult blockHitResult) {
+
+    if (world.isClient) {
+      return ActionResult.SUCCESS;
+    } else {
+      LootTable lootTable =
+          Objects.requireNonNull(world.getServer())
+              .getLootManager()
+              .getTable(getLootTableIdentifier());
+      LootContext.Builder builder =
+          new LootContext.Builder((ServerWorld) world).random(seed);
+      lootTable.generateLoot(
+          builder.build(LootContextTypes.EMPTY), stack -> dropReward(world, pos, stack));
+      return ActionResult.CONSUME;
+    }
+  }
+
+  private Identifier getLootTableIdentifier() {
+    return new Identifier(
+        MOD_ID, String.format("loot_box/%s", Registry.BLOCK.getId(this).getPath()));
+  }
+
+  private void dropReward(World world, BlockPos pos, ItemStack stack) {
+    double x = pos.getX() + 0.5f;
+    double y = pos.getY() + 1.0f - EntityType.ITEM.getHeight() / 2.0f;
+    double z = pos.getZ() + 0.5f;
+
+    if (!world.isClient() && !stack.isEmpty()) {
+      ItemEntity itemEntity = new ItemEntity(world, x, y, z, stack);
+      itemEntity.setToDefaultPickupDelay();
+      world.spawnEntity(itemEntity);
+    }
+  }
+
+  @Nullable
+  @Override
+  public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+    return null;
   }
 }
--- src/main/java/com/shmc/luckylootboxes/block/BeginnerLootBoxBlock.java
+++ src/main/java/com/shmc/luckylootboxes/block/BeginnerLootBoxBlock.java
@@ -1,8 +1,20 @@
 package com.shmc.luckylootboxes.block;
 
+import com.shmc.luckylootboxes.blockentity.BeginnerLootBoxEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
+
 public class BeginnerLootBoxBlock extends AbstractLootBoxBlock {
 
   public BeginnerLootBoxBlock(Settings settings) {
     super(settings);
+  }
+
+  @Nullable
+  @Override
+  public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+    return new BeginnerLootBoxEntity(pos, state);
   }
 }
