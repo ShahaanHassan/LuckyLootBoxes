5b1745481929cc12b7a8e26eb43470b80611f8f0
Shahaan Hassan <shahaan.hassan@gmail.com>
2022-05-06 00:39:41 +0100

    Align with base Minecraft enums
    implement StringIdentifiable

---
+++ src/main/java/com/shmc/luckylootboxes/enums/PullRarity.java
@@ -0,0 +1,38 @@
+package com.shmc.luckylootboxes.enums;
+
+import net.minecraft.util.StringIdentifiable;
+
+public enum PullRarity implements StringIdentifiable {
+    BASE("base", 0),
+    LEGENDARY("legendary", 1),
+    EPIC("epic", 4),
+    RARE("rare", 15),
+    UNCOMMON("uncommon", 20),
+    COMMON("common", 60);
+
+    private final String name;
+    private final int probability;
+
+    PullRarity(String name, int probability) {
+        this.name = name;
+        this.probability = probability;
+    }
+
+    public int getProbability() {
+        return this.probability;
+    }
+
+    public static PullRarity getRarity(int probability) {
+        for (PullRarity rarity : PullRarity.values()) {
+            if (rarity.getProbability() >= probability) {
+                return rarity;
+            }
+        }
+        return PullRarity.COMMON;
+    }
+
+    @Override
+    public String asString() {
+        return this.name;
+    }
+}
